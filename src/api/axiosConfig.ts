dc46749f32ba3a4453e2730977297e19a42c22ce
ranccat <wogurjo98@gmail.com>
2025-03-18 18:25:38 +0900

    fix: axios config

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -3,27 +3,33 @@ import axios from 'axios'
 const API_BASE_URL = "http://localhost:8080"
 
 const getToken = () => {
-  return localStorage.getItem("token")
+  if (typeof window !== "undefined") {
+    return localStorage.getItem("token")
+  }
+  return null
 }
 
 const createAxiosInstance = (basePath: string = "") => {
-  return axios.create({
+  const instance = axios.create({
     baseURL: `${API_BASE_URL}${basePath}`,
     withCredentials: true,
+    headers: {
+      "Content-Type": "application/json",
+    },
   })
+
+  instance.interceptors.request.use((config) => {
+    const token = getToken()
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  })
+
+  return instance
 }
-
-const axiosInstance = createAxiosInstance()
-
-axiosInstance.interceptors.request.use((config) => {
-  const token = getToken()
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
-  }
-  return config
-})
 
 export const authAxios = createAxiosInstance("/auth")
 export const postAxios = createAxiosInstance("/posts")
 
-export default axiosInstance
+export default createAxiosInstance()
